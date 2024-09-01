d8a5a9444d70b1ad37bc0d79a6c13a40907d465e
ArunKushhhh <arunkushwaha_230604@gmail.edu.in>
2024-09-01 23:41:49 +0530

    Project Initialised

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Canvas } from "@react-three/fiber";
+import React from "react";
+
+const App = () => {
+  return <Canvas>
+    <mesh>
+      <boxGeometry/>
+      <meshBasicMaterial />
+    </mesh>
+  </Canvas>;
+};
+
+export default App;
